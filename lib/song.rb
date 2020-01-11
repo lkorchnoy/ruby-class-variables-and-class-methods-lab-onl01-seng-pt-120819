96a7fc7d886bfdbb06ff8caea063e7e8bbb1ee61
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-01-11 14:41:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,17 @@ def self.genre_count
    
 end
 
-
+def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
 
 
 
